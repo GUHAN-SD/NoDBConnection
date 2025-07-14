5ba859cb837e9e46a1701390cc70fc1b79850eac
GUHAN-SD <guhan.official.sd@gmail.com>
2025-07-14 12:42:58 +0530

    neww

--- src/main/java/com/example/demo/controller/CController.java
+++ src/main/java/com/example/demo/controller/CController.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
+@Controller("/")
 public class CController {
 
-   
-
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
         // Show login page
         return "pages/Login"; // Login.jsp under /webapp/pages/ or wherever your prefix points
